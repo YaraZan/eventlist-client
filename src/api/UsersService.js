8662c6d6ba9426f84f77430f8db13d3a8072bc51
YaraZan <a9172607998@gmail.com>
2023-05-27 10:43:01 +0300

    add: create, login funcs

--- src/api/UsersService.js
+++ src/api/UsersService.js
@@ -3,3 +3,32 @@ export async function getUserById(id) {
 
     return await responce.json();
 }
+
+export async function createUser(name, email, password) {
+    const response = await fetch(`http://localhost/eventlist-api/api/user/create_user.php`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+        body: JSON.stringify(
+            {
+                name: name,
+                email: email,
+                password: password
+            }
+        )
+      })
+    return await response.json();
+}
+
+export async function loginUser(email, password) {
+    const response = await fetch(`http://localhost/eventlist-api/api/user/login.php`, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
+        body: JSON.stringify(
+            {
+                email: email,
+                password: password
+            }
+        )
+      })
+    return await response.json();
+}

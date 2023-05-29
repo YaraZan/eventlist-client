bb23b68bbd8a51a1229584577902a1984fc3105f
YaraZan <115586290+YaraZan@users.noreply.github.com>
2023-05-29 09:33:15 +0300

    rem: old methods replaced with axios

--- src/api/UsersService.js
+++ src/api/UsersService.js
@@ -1,34 +1,46 @@
+
 export async function getUserById(id) {
     const responce = await fetch(`http://localhost/eventlist-api/api/user/read_single.php?id=${id}`);
 
     return await responce.json();
 }
 
-export async function createUser(name, email, password) {
-    const response = await fetch(`http://localhost/eventlist-api/api/user/create_user.php`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
-        body: JSON.stringify(
-            {
-                name: name,
-                email: email,
-                password: password
-            }
-        )
-      })
-    return await response.json();
+/* export async function createUser(name, email, password) {
+    try {
+        const response = await fetch(`http://localhost/eventlist-api/api/user/create_user.php`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(
+                {
+                    name: name,
+                    email: email,
+                    password: password
+                }
+            )
+          })
+
+        return await response.json();
+    } catch (error) {
+        console.error(error); // Обрабатываем ошибку, если возникает
+        throw error;
+    }
 }
 
 export async function loginUser(email, password) {
-    const response = await fetch(`http://localhost/eventlist-api/api/user/login.php`, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json', 'Access-Control-Allow-Origin': '*'},
-        body: JSON.stringify(
-            {
-                email: email,
-                password: password
-            }
-        )
-      })
-    return await response.json();
-}
+    try {
+        const response = await fetch(`http://localhost/eventlist-api/api/user/login.php`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify(
+                {
+                    email: email,
+                    password: password
+                }
+            )
+          })
+
+        return await response.json();
+    } catch (error) {
+        console.log(error)
+    }
+} */

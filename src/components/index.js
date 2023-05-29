29dbf0b3beb95ed6cbe0419933d08f7ee6a4f43a
YaraZan <115586290+YaraZan@users.noreply.github.com>
2023-05-29 09:53:13 +0300

    add: Module

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import HomeEducation from '@/components/HomeEducation.vue'
+import HomeSocial from '@/components/HomeSocial.vue'
+import HomeBusiness from '@/components/HomeBusiness.vue'
+import HomeCulture from '@/components/HomeCulture.vue'
+import HomeScience from '@/components/HomeScience.vue'
+import HomePopular from '@/components/HomePopular.vue'
+
+export { HomeEducation, HomeSocial, HomeBusiness, HomeCulture, HomeScience, HomePopular }

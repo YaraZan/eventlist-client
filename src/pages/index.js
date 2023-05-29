783d35dc551e76375635538757f1f2d31e46aceb
YaraZan <115586290+YaraZan@users.noreply.github.com>
2023-05-29 09:54:45 +0300

    add: Module

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Home from '@/pages/Home.vue';
+import Work from '@/pages/Work.vue';
+import About from '@/pages/About.vue';
+import Login from '@/pages/Login.vue';
+import Auth from '@/pages/Auth.vue';
+import Profile from '@/pages/Profile.vue';
+
+export { 
+    Home,
+    Work,
+    About,
+    Login,
+    Auth,
+    Profile
+}
